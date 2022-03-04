6fd106cc37f2e51d098cb2eadba72613fd985cba
Tuan Anh Tran <trantuananh1912@gmail.com>
2022-03-04 22:22:03 +0900

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test
+          Learn React test 2
         </a>
       </header>
     </div>
